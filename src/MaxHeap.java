c31a0d87494411251548cad60af13a2eeb0add21
angeluskings04 <castro_angel@ciencias.unam.mx>
2023-06-05 16:55:56 -0600

    falta verificar

---
+++ src/MaxHeap.java
@@ -0,0 +1,20 @@
+
+import java.util.NoSuchElementException;
+
+public class MaxHeap <T extends Comparable<T>> extends Heap<T>{
+
+public MaxHeap(){
+    super();
+}
+
+public MaxHeap(Iterable i){
+    super(i);
+}
+
+@Override
+protected boolean comparador(T elemento1, T elemento2) {
+    return elemento2.compareTo(elemento1)<0; 
+}
+
+
+}
